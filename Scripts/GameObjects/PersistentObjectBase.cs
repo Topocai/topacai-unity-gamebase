9974cc1618f51dbaedc3327d8a5e0beb46baf4a8
topocai <topocaidev@gmail.com>
2025-07-28 20:28:44 -0300

    Refactor persistent object data management
    Reworked PersistentObjectBase to support category-based data saving and recovery, centralized object and data tracking, and event-driven data application. Added static methods for saving and recovering all objects or by category, improved data update and application logic, and introduced PersistentObjectsCategoryData struct. Enhanced extensibility and reliability of persistent object handling.

--- Scripts/GameObjects/PersistentObjectBase.cs
+++ Scripts/GameObjects/PersistentObjectBase.cs
@@ -2,48 +2,150 @@ using System.Collections.Generic;
 using Topacai.Utils.GameObjects.Unique;
 using Topacai.Utils.SaveSystem;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace Topacai.GameObjects.Persistent
 {
     public class PersistentObjectBase : UniqueIDAssigner
     {
+        public static UnityEvent<string> OnDataRecoveredEvent = new UnityEvent<string>();
         private const string DATA_KEY = "PersistentObjectsData";
 
-        [SerializeField] private string _category = "other";
+        // category -> object list
+        private static Dictionary<string, HashSet<PersistentObjectBase>> PersistentInstancesByCategory = new();
+        // category -> uniqueID -> data
+        private static Dictionary<string, Dictionary<string, IPersistentDataObject>> PersistentDataByCategory = new();
 
-        protected virtual void SaveData<T>(T dataObject) where T : IPersistentDataObject
+        [SerializeField] protected string _category = "other";
+
+        public IPersistentDataObject PersistentData { get; private set; }
+
+        private static void OnSaveGame()
         {
-            dataObject.UniqueID = GetUniqueID();
-            dataObject.Position = (SerializeableVector3)transform.position;
-            dataObject.Rotation = (SerializeableVector3)transform.eulerAngles;
-            dataObject.Scale = (SerializeableVector3)transform.localScale;
-
-            SaveController.Instance.SaveLevelDataToProfile(dataObject, GetUniqueID(), DATA_KEY + "/" + _category);
+            SaveAllObjects();
         }
 
-        protected virtual void RecoverData<T>() where T : IPersistentDataObject
+        public static void SaveAllObjects()
+        {
+            foreach (var item in PersistentInstancesByCategory)
+            {
+                SaveCategoryObjects(item.Key);
+            }
+        }
+
+        public static void SaveCategoryObjects(string category)
+        {
+            if (!PersistentInstancesByCategory.TryGetValue(category, out var objects))
+            {
+                Debug.LogWarning($"No persistent objects found for category '{category}'");
+                return;
+            }
+
+            var dataObject = new PersistentObjectsCategoryData();
+            dataObject.ObjectList = new();
+            dataObject.Category = category;
+            foreach (var item in PersistentInstancesByCategory[category])
+            {
+                item.UpdateData();
+                dataObject.ObjectList.Add(item.GetUniqueID(), item.PersistentData);
+            }
+
+            SaveController.Instance.SaveLevelDataToProfile(dataObject, category, DATA_KEY);
+        }
+
+        public static void RecoverAllObjects()
+        {
+            foreach (var item in PersistentInstancesByCategory.Keys)
+            {
+                RecoverCategory(item);
+            }
+        }
+
+        public static void RecoverCategory(string category)
         {
             if (SaveController.Instance == null) return;
-            T data = default(T);
-            bool dataExists = SaveController.Instance.GetLevelData<T>(GetUniqueID(), out data, DATA_KEY + "/" + _category);
+            PersistentObjectsCategoryData data = default(PersistentObjectsCategoryData);
+            bool dataExists = SaveController.Instance.GetLevelData<PersistentObjectsCategoryData>(category, out data, DATA_KEY);
 
-            if (!dataExists) return;
-            transform.position = (Vector3)data.Position;
-            transform.eulerAngles = (Vector3)data.Rotation;
-            transform.localScale = (Vector3)data.Scale;
+            if(!dataExists) return;
+
+            PersistentDataByCategory[data.Category] = data.ObjectList;
+            OnDataRecoveredEvent.Invoke(data.Category);
         }
 
-        protected virtual void Start()
+        public static IPersistentDataObject GetObjectData(string uniqueID, string category)
         {
-            RecoverData<PersistentObjectData>();
+            if(PersistentDataByCategory.TryGetValue(category, out var cat))
+            {
+                if (cat.TryGetValue(uniqueID, out var obj))
+                {
+                    return obj;
+                }
+            }
+
+            Debug.LogWarning($"Object data not found for ID: {uniqueID} in category: {category}");
+            return null;
+        }
+
+        protected override void Awake()
+        {
+            base.Awake();
+            if(!PersistentInstancesByCategory.ContainsKey(_category))
+            {
+                PersistentInstancesByCategory.Add(_category, new());
+                RecoverCategory(_category);
+            }
+
+            PersistentInstancesByCategory[_category].Add(this);
+
             SaveController.OnSaveGameEvent.AddListener(OnSaveGame);
         }
 
-        protected virtual void OnEnable()
+        public void UpdateData()
         {
+            if(PersistentData == null)
+            {
+                PersistentData = new PersistentObjectData();
+                Debug.LogWarning("PersistentData is null");
+            }
+            OnUpdateData();
+            PersistentData.UniqueID = GetUniqueID();
+            PersistentData.Position = (SerializeableVector3)transform.position;
+            PersistentData.Rotation = (SerializeableVector3)transform.eulerAngles;
+            PersistentData.Scale = (SerializeableVector3)transform.localScale;
+        }
+
+        public void ApplyData()
+        {
+            if(PersistentData == null) return;
+            transform.position = (Vector3)PersistentData.Position;
+            transform.eulerAngles = (Vector3)PersistentData.Rotation;
+            transform.localScale = (Vector3)PersistentData.Scale;
+            OnApplyData();
+        }
+
+        protected virtual void OnApplyData() { }
+
+        public virtual void OnUpdateData() { }
+
+        protected virtual void Start()
+        {
+            if (PersistentData == null)
+            {
+                PersistentData = new PersistentObjectData();
+            }
+            PersistentData = GetObjectData(GetUniqueID(), _category);
+            ApplyData();
+            OnDataRecoveredEvent.AddListener(OnDataRecovered);
+            
+        }
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
             if (Application.isPlaying)
             {
-                //SaveController.OnSaveGameEvent.AddListener(OnSaveGame);
+                OnDataRecoveredEvent.AddListener(OnDataRecovered);
             }
         }
 
@@ -51,24 +153,31 @@ namespace Topacai.GameObjects.Persistent
         {
             if (Application.isPlaying)
             {
-                SaveController.OnSaveGameEvent.RemoveListener(OnSaveGame);
+                OnDataRecoveredEvent.RemoveListener(OnDataRecovered);
             }
         }
 
-
-        protected virtual void OnSaveGame()
+        protected virtual void OnDataRecovered(string category)
         {
-            SaveData(new PersistentObjectData());
+            if(category == _category)
+            {
+                PersistentData = GetObjectData(GetUniqueID(), _category);
+                ApplyData();
+            }
         }
     }
 
+    public struct PersistentObjectsCategoryData
+    {
+        public string Category { get; set; }
+        public Dictionary<string, IPersistentDataObject> ObjectList { get; set; }
+    }
     public interface IPersistentDataObject
     {
         public string UniqueID { get; set; }
         public SerializeableVector3 Position { get; set; }
         public SerializeableVector3 Rotation { get; set; }
         public SerializeableVector3 Scale { get; set; }
-
     }
 
     public struct PersistentObjectData : IPersistentDataObject
@@ -77,5 +186,20 @@ namespace Topacai.GameObjects.Persistent
         public SerializeableVector3 Position { get; set; }
         public SerializeableVector3 Rotation { get; set; }
         public SerializeableVector3 Scale { get; set; }
+
+        public override int GetHashCode() => UniqueID?.GetHashCode() ?? 0;
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is PersistentObjectData other))
+            {
+                if (obj is string)
+                {
+                    return UniqueID == (string)obj;
+                }
+                return false;
+            }
+            return UniqueID == other.UniqueID;
+        }
     }
 }
