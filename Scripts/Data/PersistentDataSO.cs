eb59c39fec70b2099e2ca01b99f4e10d013ea704
topocai <topocaidev@gmail.com>
2025-10-02 11:53:51 -0300

    Add PersistentDataSO base class for save system
    Introduces PersistentDataSO, a ScriptableObject base class to enable persistent data saving between sessions. It subscribes to SaveController events and marks itself dirty in the editor when saving.

---
+++ Scripts/Data/PersistentDataSO.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEditor;
+
+namespace Topacai.Utils.SaveSystem
+{
+    /// <summary>
+    /// A ScriptableObject class that would save the data between sessions
+    /// use as base class to made a scriptable object persistent
+    /// </summary>
+    public class PersistentDataSO : ScriptableObject
+    {
+        private bool isSuscribed = false;
+        protected virtual void OnValidate()
+        {
+            if (!isSuscribed)
+            {
+                SaveController.OnSaveGameEvent.AddListener(SaveData);
+                isSuscribed = true;
+            }
+        }
+        public virtual void SaveData()
+        {
+            EditorUtility.SetDirty(this);
+        }
+    }
+}
