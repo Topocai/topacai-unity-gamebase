d1f4a2642235ba0ffa426288bc9afebd059f7c2b
topocai <topocaidev@gmail.com>
2025-11-21 20:38:58 -0300

    GameManager now is a Singleton
    GameManager was refactorized into a Singleton monobehaviour (using Singleton<T> class), now accepts implementtation from other files by changed it to a partial class.
    
    Implemented events to handle new scene start loading (additive) and finished to load (asyncroniusly), same behaviour for a Single mode scene loading, but different event in order to support custom behaviours on Scene Switching and Scene Addition

--- Scripts/Managers/GameManager.cs
+++ Scripts/Managers/GameManager.cs
@@ -1,26 +1,89 @@
 using EditorAttributes;
+
+using System;
 using System.Collections;
 using System.Collections.Generic;
+
+using Topacai.Utils.GameObjects;
+
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 
 namespace Topacai.Managers.GameManager
 {
-    public class GameManager : MonoBehaviour
+    public partial class GameManager : Singleton<GameManager>
     {
-        [SerializeField, OnValueChanged(nameof(OnFrameRateChanged))] private int frameRate = 60;
-
-        private void OnFrameRateChanged() => SetFrameRate(frameRate);
-        public void SetFrameRate(int frameRate)
+        public partial class OnSceneArgs : System.EventArgs
         {
-            Application.targetFrameRate = frameRate;
+            public Scene ActualScene;
+            public Scene NewScene;
+
+            public LoadSceneMode NewSceneType;
+
+            public Scene[] NonActiveScenes;
         }
 
+        public static UnityEvent<object, OnSceneArgs> OnLoadingScene = new();
+        public static UnityEvent<object, OnSceneArgs> OnSwitchingScene = new();
+
+        public static UnityEvent<OnSceneArgs> OnSceneLoaded = new();
+        public static UnityEvent<OnSceneArgs> OnSceneSwitched = new();
+
+        public static UnityEvent<object, OnSceneArgs> OnSceneUnloaded = new();
+
+#if UNITY_EDITOR
         public void DebugLog(string message) => Debug.Log(message);
+        public void ShowLogOnScreen(object sender, string msg, float duration= 0.1f) => Topacai.TDebug.Debugcanvas.Instance.AddTextToDebugLog(sender!=null ? $"{sender.ToString()}" : "GM Log", msg, duration);
+#endif
 
         private void Start()
         {
             Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible = false;
+        }
+
+        private IEnumerator WaitingForSceneLoad(string sceneName, AsyncOperation op, Action<string> action)
+        {
+            yield return new WaitUntil(() => op.isDone);
+
+            var scene = SceneManager.GetSceneByName(sceneName);
+
+            if (scene.isLoaded) action.Invoke(sceneName);
+        }
+
+        private void OnAdditiveLoaded(string sceneName)
+        {
+            OnSceneLoaded?.Invoke(new OnSceneArgs() { NewScene = SceneManager.GetSceneByName(sceneName) });
+        }
+
+        private void OnSwitchLoaded(string sceneName)
+        {
+            OnSceneSwitched?.Invoke(new OnSceneArgs() { NewScene = SceneManager.GetSceneByName(sceneName) });
+        }
+
+        public void LoadScene(object sender, string sceneName, LoadSceneMode mode = LoadSceneMode.Additive)
+        {
+            var scene = SceneManager.GetSceneByName(sceneName);
+
+            var args = new OnSceneArgs()
+            {
+                ActualScene = SceneManager.GetActiveScene(),
+                NewScene = scene,
+                NewSceneType = mode
+            };
+
+            if (mode == LoadSceneMode.Single)
+            {
+                OnSwitchingScene?.Invoke(sender, args);
+            }
+            else
+            {
+                OnLoadingScene?.Invoke(sender, args);
+            }
+
+            var asyncOp = SceneManager.LoadSceneAsync(sceneName, mode);
+            StartCoroutine(WaitingForSceneLoad(sceneName, asyncOp, mode == LoadSceneMode.Additive ? OnAdditiveLoaded : OnSwitchLoaded));
         }
     }
 }
