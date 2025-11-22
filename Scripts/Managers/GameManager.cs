6f7f382658f11a469bb2aa4c15af25b53efcf62e
topocai <topocaidev@gmail.com>
2025-11-21 21:13:25 -0300

    Refactor scene loading and unloading logic
    Introduces methods for loading and unloading sets of scenes, adds new events for unloading scenes, and updates OnSceneArgs to use TargetScene and string[] NonActiveScenes. Improves coroutine handling for scene load/unload operations and refactors event invocation

--- Scripts/Managers/GameManager.cs
+++ Scripts/Managers/GameManager.cs
@@ -17,11 +17,11 @@ namespace Topacai.Managers.GameManager
         public partial class OnSceneArgs : System.EventArgs
         {
             public Scene ActualScene;
-            public Scene NewScene;
+            public Scene TargetScene;
 
             public LoadSceneMode NewSceneType;
 
-            public Scene[] NonActiveScenes;
+            public string[] NonActiveScenes;
         }
 
         public static UnityEvent<object, OnSceneArgs> OnLoadingScene = new();
@@ -30,7 +30,8 @@ namespace Topacai.Managers.GameManager
         public static UnityEvent<OnSceneArgs> OnSceneLoaded = new();
         public static UnityEvent<OnSceneArgs> OnSceneSwitched = new();
 
-        public static UnityEvent<object, OnSceneArgs> OnSceneUnloaded = new();
+        public static UnityEvent<object, OnSceneArgs> OnUnloadingScene = new();
+        public static UnityEvent<OnSceneArgs> OnSceneUnloaded = new();
 
 #if UNITY_EDITOR
         public void DebugLog(string message) => Debug.Log(message);
@@ -43,47 +44,117 @@ namespace Topacai.Managers.GameManager
             Cursor.visible = false;
         }
 
-        private IEnumerator WaitingForSceneLoad(string sceneName, AsyncOperation op, Action<string> action)
+        private IEnumerator WaitForSceneLoad(string sceneName, AsyncOperation op, Action<string, OnSceneArgs> action, OnSceneArgs args = null)
         {
             yield return new WaitUntil(() => op.isDone);
 
             var scene = SceneManager.GetSceneByName(sceneName);
 
-            if (scene.isLoaded) action.Invoke(sceneName);
+            if (scene.isLoaded) action.Invoke(sceneName, args);
         }
 
-        private void OnAdditiveLoaded(string sceneName)
+        private IEnumerator WaitForSceneUnload(string sceneName, AsyncOperation op, Action<string, OnSceneArgs> action, OnSceneArgs args = null)
         {
-            OnSceneLoaded?.Invoke(new OnSceneArgs() { NewScene = SceneManager.GetSceneByName(sceneName) });
-        }
+            yield return new WaitUntil(() => op.isDone);
 
-        private void OnSwitchLoaded(string sceneName)
-        {
-            OnSceneSwitched?.Invoke(new OnSceneArgs() { NewScene = SceneManager.GetSceneByName(sceneName) });
-        }
-
-        public void LoadScene(object sender, string sceneName, LoadSceneMode mode = LoadSceneMode.Additive)
-        {
             var scene = SceneManager.GetSceneByName(sceneName);
 
+            if (!scene.isLoaded) action.Invoke(sceneName, args);
+        }
+
+        private void OnAdditiveLoaded(string sceneName, OnSceneArgs args)
+        {
+            OnSceneLoaded?.Invoke(args ?? new OnSceneArgs() { TargetScene = SceneManager.GetSceneByName(sceneName) });
+        }
+
+        private void OnSceneFinishUnload(string sceneName, OnSceneArgs args)
+        {
+            OnSceneUnloaded?.Invoke(args ?? new OnSceneArgs() { TargetScene = SceneManager.GetSceneByName(sceneName) });
+        }
+
+        private void OnSwitchLoaded(string sceneName, OnSceneArgs args)
+        {
+            OnSceneSwitched?.Invoke(args ?? new OnSceneArgs() { TargetScene = SceneManager.GetSceneByName(sceneName) });
+        }
+
+        public void LoadSetOfScenes(object sender, string[] scenes)
+        {
             var args = new OnSceneArgs()
             {
                 ActualScene = SceneManager.GetActiveScene(),
-                NewScene = scene,
+
+                NonActiveScenes = scenes
+            };
+
+            foreach (var scene in scenes)
+            {
+                args.TargetScene = SceneManager.GetSceneByName(scene);
+
+                LoadScene(sender, scene, LoadSceneMode.Additive, args);
+            }
+        }
+
+        public void UnloadSetOfScenes(object sender, string[] scenes)
+        {
+            var args = new OnSceneArgs()
+            {
+                ActualScene = SceneManager.GetActiveScene(),
+
+                NonActiveScenes = scenes
+            };
+
+            foreach (var scene in scenes)
+            {
+                args.TargetScene = SceneManager.GetSceneByName(scene);
+
+                UnloadScene(sender, scene, args);
+            }
+        }
+
+        public void UnloadScene(object sender, string sceneName, OnSceneArgs args = null)
+        {
+            var scene = SceneManager.GetSceneByName(sceneName);
+
+            if (!scene.IsValid() || !scene.isLoaded)
+            {
+                return;
+            }
+
+            var a = args ?? new OnSceneArgs() { ActualScene = SceneManager.GetActiveScene(), TargetScene = scene };
+            OnUnloadingScene?.Invoke(sender, a);
+
+            var asyncOp = SceneManager.UnloadSceneAsync(scene);
+
+            StartCoroutine(WaitForSceneUnload(sceneName, asyncOp, OnSceneFinishUnload, a));
+        }
+
+        public void LoadScene(object sender, string sceneName, LoadSceneMode mode = LoadSceneMode.Additive, OnSceneArgs args = null)
+        {
+            var scene = SceneManager.GetSceneByName(sceneName);
+
+            if (scene.isLoaded)
+            {
+                return;
+            }
+
+            var a = args ?? new OnSceneArgs()
+            {
+                ActualScene = SceneManager.GetActiveScene(),
+                TargetScene = scene,
                 NewSceneType = mode
             };
 
             if (mode == LoadSceneMode.Single)
             {
-                OnSwitchingScene?.Invoke(sender, args);
+                OnSwitchingScene?.Invoke(sender, a);
             }
             else
             {
-                OnLoadingScene?.Invoke(sender, args);
+                OnLoadingScene?.Invoke(sender, a);
             }
 
             var asyncOp = SceneManager.LoadSceneAsync(sceneName, mode);
-            StartCoroutine(WaitingForSceneLoad(sceneName, asyncOp, mode == LoadSceneMode.Additive ? OnAdditiveLoaded : OnSwitchLoaded));
+            StartCoroutine(WaitForSceneLoad(sceneName, asyncOp, mode == LoadSceneMode.Additive ? OnAdditiveLoaded : OnSwitchLoaded, args));
         }
     }
 }
