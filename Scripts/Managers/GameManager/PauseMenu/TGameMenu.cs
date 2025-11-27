1a9bda5f34005c50d999ab231d4216fa364d41de
topocai <topocaidev@gmail.com>
2025-11-27 11:46:10 -0300

    Add button click event handling to TGameMenu
    Introduces OnAnyButtonClicked UnityEvent to handle button clicks in the pause menu.

--- Scripts/Managers/GameManager/PauseMenu/TGameMenu.cs
+++ Scripts/Managers/GameManager/PauseMenu/TGameMenu.cs
@@ -1,16 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
 using Topacai.Utils.GameMenu;
 
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 namespace Topacai.Managers.GM.PauseMenu
 {
     public class TGameMenu : MonoBehaviour
     {
+        public UnityEvent<ClickEvent> OnAnyButtonClicked = new();
+
         [SerializeField] private UIMenu _menu = new();
         [SerializeField] private InputAction _backInput;
 
         [SerializeField] private Transform _gameMenuTransform;
+
+        private List<Button> _buttons = new();
+
+        public UIMenu Menu => _menu;
 
         public void Init()
         {
@@ -23,15 +35,40 @@ namespace Topacai.Managers.GM.PauseMenu
             _menu.SetNode(UIMenu.GetTreeFromTransform(_gameMenuTransform));
 
             _backInput.Enable();
+            _backInput.performed += _ => BackListener();
 
-            _backInput.performed += _ => _menu.BackExitAction();
+            CheckForButtons();
+
+            _menu.OnMenuChanged.AddListener(CheckForButtons);
+        }
+
+        public void BackListener()
+        {
+            foreach (var button in _buttons)
+            {
+                button.UnregisterCallback<ClickEvent>(OnButtonClicked);
+            }
+            _menu.BackExitAction();
+        }
+
+        public void CheckForButtons()
+        {
+            var b = _menu.CurrentNode?.View.Document.rootVisualElement?.Query<Button>().ToList();
+
+            if (_buttons != null)
+            {
+                foreach (var button in b)
+                    button.RegisterCallback<ClickEvent>(OnButtonClicked);
+                _buttons = b;
+            }
         }
 
         private void OnEnable()
         {
             _backInput.Enable();
-
             _gameMenuTransform.gameObject.SetActive(true);
+
+            CheckForButtons();
         }
 
         private void OnDisable()
@@ -39,6 +76,11 @@ namespace Topacai.Managers.GM.PauseMenu
             _backInput.Disable();
 
             _gameMenuTransform.gameObject.SetActive(false);
+        }
+
+        private void OnButtonClicked(ClickEvent args)
+        {
+            OnAnyButtonClicked?.Invoke(args);
         }
     }
 }
