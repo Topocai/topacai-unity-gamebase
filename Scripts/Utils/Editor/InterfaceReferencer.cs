a931bae30b7f25509682ca4f9a6109ef736f2111
topocai <topocaidev@gmail.com>
2025-12-05 15:48:42 -0300

    Add generic InterfaceReference utility for Unity inspector
    Introduces InterfaceReference<T> to allow referencing interface types in the Unity inspector

---
+++ Scripts/Utils/Editor/InterfaceReferencer.cs
@@ -0,0 +1,57 @@
+
+using UnityEngine;
+using UnityEngine.UIElements;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace Topacai.Utils.Editor
+{
+    [System.Serializable]
+    /// <summary>
+    /// Implements an interface referencer drawer in inspector to drag and drop a
+    /// specific interface type (implemented on a class that can be referenced by Unity.Object type)
+    /// </summary>
+    public class InterfaceReference<T> where T : class
+    {
+        [SerializeField] private Object _component;
+        public T Value => _component as T;
+        public Object Component => _component;
+    }
+#if UNITY_EDITOR
+    [CustomPropertyDrawer(typeof(InterfaceReference<>))]
+    public class InterfaceReferenceDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            SerializedProperty componentProp = property.FindPropertyRelative("_component");
+
+            EditorGUI.BeginProperty(position, label, property);
+
+            Object obj = EditorGUI.ObjectField(position, label, componentProp.objectReferenceValue, typeof(Component), true);
+
+            if (obj != null)
+            {
+                var interfaceType = fieldInfo.FieldType.GetGenericArguments()[0];
+
+                if (interfaceType.IsAssignableFrom(obj.GetType()))
+                {
+                    componentProp.objectReferenceValue = obj;
+                }
+                else
+                {
+                    Debug.LogError($"{obj.name} does not implement {interfaceType.Name}");
+                    componentProp.objectReferenceValue = null;
+                }
+            }
+            else
+            {
+                componentProp.objectReferenceValue = null;
+            }
+
+            EditorGUI.EndProperty();
+        }
+#endif
+    }
+}
