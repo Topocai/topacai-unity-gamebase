07cb1ca2d5b1317b6955b95c48a8300463c3f063
Luna Pocai <lunapocai@gmail.com>
2026-03-01 19:39:12 -0300

    set height on crouch now implements an interpolation with time to scale

--- Actors/Scripts/Player/Movement/FirstPerson/Components/Crouch/CrouchComponent.cs
+++ Actors/Scripts/Player/Movement/FirstPerson/Components/Crouch/CrouchComponent.cs
@@ -8,6 +8,7 @@ using Topacai.Player.Movement.Components;
 using Topacai.Inputs;
 using Topacai.Utils;
 using Topacai.TDebug;
+using System.Collections;
 
 namespace Topacai.Player.Movement.Firstperson.Components.Crouch
 {
@@ -127,44 +128,73 @@ namespace Topacai.Player.Movement.Firstperson.Components.Crouch
 
         #endregion
 
+        private Coroutine _heightRoutine;
 
-
-        public virtual void SetHeightScale(float ratio)
+        public void SetHeightScale(float ratio, float duration = 0.15f)
         {
+            if (_heightRoutine != null)
+                StopCoroutine(_heightRoutine);
+
+            _heightRoutine = StartCoroutine(HeightScaleRoutine(ratio, duration));
+        }
+
+        private IEnumerator HeightScaleRoutine(float ratio, float duration)
+        {
+            /// detection and interpolation mode are handled because sometimes crouch is
+            /// perform on air or not able to scale due to rigidbody physics
             RigidbodyInterpolation originalInter = _movement.Rigidbody.interpolation;
             CollisionDetectionMode originalDetection = _movement.Rigidbody.collisionDetectionMode;
+
             _movement.Rigidbody.collisionDetectionMode = CollisionDetectionMode.Discrete;
             _movement.Rigidbody.interpolation = RigidbodyInterpolation.None;
 
+            // gets the pivot for scale from the bottom of the player using the bound of the capsule
+            // and calculate the target new scale by ratio, and applying it doing an interpolation
+
             Bounds bounds = _movement.PlayerBrain.GetComponent<Renderer>().bounds;
-            Vector3 _crouchPivotPos = new Vector3(bounds.center.x, bounds.min.y, bounds.center.z);
+            Vector3 pivot = new Vector3(bounds.center.x, bounds.min.y, bounds.center.z);
 
             float targetY = _initialPlayerScale.y * ratio;
             Vector3 targetScale = new Vector3(_initialPlayerScale.x, targetY, _initialPlayerScale.z);
 
-            Vector3 currentScale = _movement.transform.localScale;
-            Vector3 scaleFactor = new Vector3(
-                targetScale.x / currentScale.x,
-                targetScale.y / currentScale.y,
-                targetScale.z / currentScale.z
-            );
+            Vector3 startScale = _movement.transform.localScale;
 
-            Transforms.ScaleRelativeToPivot(_movement.transform, scaleFactor, _crouchPivotPos);
+            float time = 0f;
+            while (time < 1f)
+            {
+                time += Time.deltaTime / duration;
+                float k = Mathf.SmoothStep(0f, 1f, time);
+
+                Vector3 current = Vector3.Lerp(startScale, targetScale, k);
+
+                Vector3 scaleFactor = new Vector3(
+                    current.x / _movement.transform.localScale.x,
+                    current.y / _movement.transform.localScale.y,
+                    current.z / _movement.transform.localScale.z
+                );
+
+                Transforms.ScaleRelativeToPivot(_movement.transform, scaleFactor, pivot);
+
+                // keep the size of childs that are marked to preserve their scale
+                for (int i = 0; i < _preserveScale.Length; i++)
+                {
+                    if (_preserveScale[i] != null)
+                    {
+                        Vector3 newScale = _initialChildrenScales[i];
+                        newScale.y = _initialChildrenScales[i].y / Mathf.Lerp(1f, ratio, k);
+                        _preserveScale[i].localScale = newScale;
+                    }
+                }
+
+                yield return null;
+            }
 
             _movement.Rigidbody.interpolation = originalInter;
             _movement.Rigidbody.collisionDetectionMode = originalDetection;
 
-            /// calculate new scale for any child that are marked to preserve their original size
-            for (int i = 0; i < _preserveScale.Length; i++)
-            {
-                if (_preserveScale[i] != null)
-                {
-                    Vector3 newScale = _initialChildrenScales[i];
-                    newScale.y = _initialChildrenScales[i].y / ratio;
-                    _preserveScale[i].localScale = newScale;
-                }
-            }
+            _heightRoutine = null;
         }
+
 
         public virtual void Crouch()
         {
@@ -172,8 +202,6 @@ namespace Topacai.Player.Movement.Firstperson.Components.Crouch
             _isCrouching = !_isCrouching;
 
             Debugcanvas.Instance.AddTextToDebugLog("crouch", $"{_isCrouching}");
-
-
         }
 
         public virtual void Crouch(bool state)
